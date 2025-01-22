f0b5a409be71457f85cefdaea86865dcf3af355c
SachiniPinnaduwa <sachinipinnaduwa2@gmail.com>
2025-01-22 12:23:54 +0530

    CreateExpressServer

---
+++ src/index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const dotenv = require("dotenv").config();
+
+const app = express();
+
+//Middleware
+app.use(express.json());
+
+//Routes
+
+//Start the server
+const PORT = process.env.PORT || 7002;
+app.listen(PORT, () => {
+  console.log(`Server is running at port ${PORT}`);
+});
