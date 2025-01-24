b24881d0bab7ca74ccf08b773517fd473c00ddc7
SachiniPinnaduwa <sachinipinnaduwa2@gmail.com>
2025-01-24 08:41:53 +0530

    userRoutes

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const dotenv = require("dotenv").config();
+
 const dbConnect = require("./config/dbConnect");
 const authRoutes = require("./routes/authRoutes");
+const userRoutes = require("./routes/userRoutes");
 
 dbConnect();
 
@@ -12,6 +14,7 @@ app.use(express.json());
 
 //Routes
 app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes);
 
 //Start the server
 const PORT = process.env.PORT || 7002;
